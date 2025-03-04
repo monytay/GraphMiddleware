4e0c2903da3798fae6fae5ad8b74a5050a859a18
Antonio Toni Majstorovic <155436092+monytay@users.noreply.github.com>
2025-03-04 17:04:00 +0100

    Update and rename middleware.py to app.py

--- app.py
+++ app.py
@@ -1,32 +1,33 @@
-from dotenv import load_dotenv
 import os
 import requests
 import json
 import logging 
 import azure.functions as func
 
-load_dotenv()
+tenantID="test"
+clientID="test2"
+clientSecret="secret"
+scope = "https://www.graph.microsoft.com/.default" 
 
-tenantID = os.getenv("tenantID")
-clientID = os.getenv('clientID')
-client_secret = os.getenv("clientSecret")
-scope = os.getenv("scope")
 
 def getAccessToken():
     try:
       data = {
         "grant_type" : "client_credentials",
-        "clientID" : clientID,
-        "tenantID" : tenantID,
+        "client_id" : clientID,
+        "tenant_id" : tenantID,
+        "clientSecret" : clientSecret,
         "scope" : scope
       }
-      response = requests.post(f'https://login.microsoftonline.com/{tenantID}/oauth2/token', data=data)
+      response = requests.post(f'http://localhost:8002/getToken/', data=data)
       errorMessage="Token retrieval error"
 
-      if response.status_code() == 200:
-        tokenInfo = response.json(),
-        return tokenInfo.get('access_token')
+      if response.status_code == 200:
+        tokenInfo = response.json()
+        print("AccessToken:",tokenInfo.get('AccessToken'))
+        return tokenInfo.get('AccessToken')
     except:
+      print(errorMessage)
       return errorMessage
 
 def getPrinters():
@@ -37,7 +38,7 @@ def getPrinters():
       "Authorization" : f'Bearer{token}',
       "Content-Type" : "application/json"
     }
-    response = request(f"https://graph.microsoft.com/PrinterShare.Read.All",headers=headers)
+    response = requests.get(f"https://graph.microsoft.com/PrinterShare.Read.All",headers=header)
     errorMessage="UniversalPrint API error"
 
     if response.status() == 200:
