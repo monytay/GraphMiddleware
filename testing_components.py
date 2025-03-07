5452fdcb984362e2b9d42e2ae5d3466d16101103
Antonio Toni Majstorovic <155436092+monytay@users.noreply.github.com>
2025-03-07 15:29:32 +0100

    Update testing_components.py

--- testing_components.py
+++ testing_components.py
@@ -1,18 +1,28 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+class TokenRequest (BaseModel):
+    tenant_id:str
+    client_id:str
+    client_secret:str
 
 app = FastAPI()
 
-@app.get("/getToken/")
-async def getToken(tenant_id: str, client_id:str, clientSecret: str):
+@app.post("/getToken/")
+async def getToken(token_request: TokenRequest):
     valid_tenantID="test"
     valid_clientID="test2"
     valid_clientSecret="secret"
 
-    if valid_clientID == client_id and valid_tenantID == tenant_id and valid_clientSecret == clientSecret:
-        return {"AccessToken":"FakeToken"}
+    if (valid_clientID == token_request.client_id and 
+        valid_tenantID == token_request.tenant_id and
+        valid_clientSecret == token_request.client_secret):
+        return {"access_token":"FakeToken"}
     else:
         raise HTTPException(status_code=400,detail="Invalid clientID or tenantID or clientSecret")
 
 
+
+    
 
     
