10f017eb306c2e606daa14f753e6da29db89c5d4
Antonio Toni Majstorovic <155436092+monytay@users.noreply.github.com>
2025-03-24 16:20:03 +0100

    Update getPrinters.py

--- getPrinters.py
+++ getPrinters.py
@@ -5,16 +5,29 @@ def getPrinters():
   try:
     
     token = getAccessToken()
+    if not token:
+      print("Failed to retrieve token")
+      return None
+
     header = {
       "Authorization" : f'Bearer {token}',
       "Content-Type" : "application/json"
     }
-    response = requests.get(f"https://graph.microsoft.com/PrinterShare.Read.All",headers=header)
+
+    response = requests.get(f"https://graph.microsoft.com/beta/print/printers",headers=header)
     errorMessage="UniversalPrint API error"
 
     if response.status_code == 200:
       printersInfo = response.json()
+      print(printersInfo)
       return printersInfo 
+    else:
+      print(errorMessage, response.text, "Response status code: ", response.status_code)
   except Exception as e:
     print(errorMessage,e)
-  return errorMessage
+  return None
+
+if __name__ == "__main__":
+    print("Starting app...")
+    printers = getPrinters()
+    print("Retrieved Printers Info:", printers)
