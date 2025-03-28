5e01ad7f6af3546a0150408cb527b316881e4267
Antonio Toni Majstorovic <155436092+monytay@users.noreply.github.com>
2025-03-28 13:50:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,16 @@ import json
 import logging 
 import azure.functions as func
 
+access_token = None
+
 def getAccessToken():
-    tenantID="Real id from azzure"
-    clientID="real client id from azure"
-    clientSecret="real client secret from azure"
+    global access_token
+    if access_token:
+      return access_token
+
+    tenantID="test"
+    clientID="test"
+    clientSecret="test"
     scope = "https://www.graph.microsoft.com/.default" 
 
     url = f"https://login.microsoftonline.com/{tenantID}/oauth2/v2.0/token"
@@ -22,19 +28,20 @@ def getAccessToken():
     try:
       response = requests.post(url, data = data)
       errorMessage="Token retrieval error"
-
-      print("Sending request to get access token...")  # Debugging line
-      print("Request payload:", data)  # Print the data being sent
-
+      
       if response.status_code == 200:
         tokenInfo = response.json()
-        print("AccessToken:",tokenInfo.get('access_token'))
+        access_token = tokenInfo.get("access_token")
         return tokenInfo.get('access_token')
       else:
         print(errorMessage, response.text, "Response status code:", response.status_code)
     except Exception as e:
       print(errorMessage,e)
     return errorMessage
+
+
+
+
 
 
 
